1821c44c10c46f0012f7a3c340143aa227f69221
Niko Kapfer-Rupp <nikolaus.kapfer.rupp@gmail.com>
2024-02-05 12:22:16 +0100

    added 'calculator.js', 'eslintrc.yml' and 'Pseudocode'

--- calculator.js
+++ calculator.js
@@ -1,37 +1,53 @@
-// Ask the user for the first number.
-// Ask the user for the second number.
-// Ask the user for an operation to perform.
-// Perform the operation on the two numbers.
-// Print the result to the terminal.
-
 const readline = require('readline-sync');
 
-console.log("Welcome to Calculator!");
+function prompt(msg) {
+  console.log(`=> ${msg}`)
+};
 
-console.log("What's the first number?");
+function invalidNumber(num) {
+   return num.trimStart() === '' || Number.isNaN(Number(num));
+};
+
+prompt('Welcome to Calculator!');
+
+prompt("What's the first number?");
 let firstNumber = readline.question();
 
-console.log("What's the second number?");
+while(invalidNumber(firstNumber)) {
+  prompt("Hmmm... that doesn't look like a valid number.");
+  firstNumber = readline.question();
+};
+
+prompt("What's the second number?");
 let secondNumber = readline.question();
 
-console.log(
-    "What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide"
-  );
+while(invalidNumber(secondNumber)) {
+  prompt("Hmmm... that doesn't look like a valid number.");
+  secondNumber = readline.question();
+};
+
+prompt('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide');
 let operation = readline.question();
 
+while(!['1', '2', '3', '4'].includes(operation)) {
+  prompt("Must choose 1,2,3, or 4.");
+  operation = readline.question();
+};
+
 let output;
-if (operation === "1") {
-    // '1' represents addition
+switch(operation) {
+  case '1': // '1' represents addition
     output = Number(firstNumber) + Number(secondNumber);
-} else if (operation === "2") {
-    // '2' represents subtraction
+    break;
+  case '2': // '2' represents subtraction
     output = Number(firstNumber) - Number(secondNumber);
-} else if (operation === "3") {
-    // '3' represents multiplication
+    break;
+  case '3': // '3' represents multiplication
     output = Number(firstNumber) * Number(secondNumber);
-} else if (operation === "4") {
-    // '4' represents division
+    break;
+  case '4': // '4' represents division
     output = Number(firstNumber) / Number(secondNumber);
+    break;
 }
-  
-  console.log(`The result is: ${output}`);
+
+prompt(`The result is: ${output}`);
